b9f9800c2a8019942f128cee9db62becc5b631c0
maersux <marcel@gertsch.be>
2024-07-29 11:02:26 +0200

    fix: incorrect user count display

--- src/components/User/UserList.tsx
+++ src/components/User/UserList.tsx
@@ -8,9 +8,12 @@ import { useUserListData } from '@/hooks/useUserListData';
 import { UserListProps } from '@/misc/Interfaces';
 import { Button, Divider } from '@nextui-org/react';
 import { FC } from 'react';
+import { filterUsers } from '@/utils/user';
 
 export const UserList: FC<UserListProps> = ({ type, role, user }) => {
   const { users, isLoading, error, reload } = useUserListData(user, type, role);
+
+  const filteredUsers = filterUsers(users);
 
   return (
     <div className="relative p-4 pr-2 border-1 border-primary-700 rounded-lg">
@@ -30,13 +33,12 @@ export const UserList: FC<UserListProps> = ({ type, role, user }) => {
       {!isLoading && !error && (
         <>
           <p className="text-center mt-1 mb-2 text-large">
-            {users.length} {role}
+            {filteredUsers.length} {role}
           </p>
           <div className="md:max-h-[32rem] overflow-y-auto flex flex-col gap-4">
-            {users.map((user, index) => {
-              if (user.ignored) return;
-              return <UserListItem key={index} user={user} />;
-            })}
+            {filteredUsers.map((user, index) => (
+              <UserListItem key={index} user={user} />
+            ))}
           </div>
         </>
       )}
