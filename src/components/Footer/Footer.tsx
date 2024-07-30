6b3817ba994b39606e9e2c680c0f6c88d346e958
cvk3 <71596651+relaxoo@users.noreply.github.com>
2024-07-30 08:16:10 +0200

    feat: added status to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
 
   return (
     <footer className="bg-primary-800 py-4 text-center">
-      <p>&copy; {year} - not affiliated with twitch interactive - <a className="underline" href={'/api/docs'}>api docs</a> - <a className="underline" href={'/tos'}>tos</a></p>
+      <p>&copy; {year} - not affiliated with twitch interactive - <a className="underline" href={'/api/docs'}>api docs</a> - <a className="underline" href={'/tos'}>tos</a> - <a className="underline" href={'https://status.modchecker.com/'}>status</a></p>
     </footer>
   );
 };
