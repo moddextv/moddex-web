0c9757158547615f8e0a7ba2e5af67629693e8b1
maersux <marcel@gertsch.be>
2024-07-31 13:36:33 +0200

    style: added spaces

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,10 @@ export const Footer = () => {
     <footer className="bg-primary-800 py-4 text-center">
       <p>
         <span>&copy; {year}</span> -
-        <span>not affiliated with twitch interactive</span> -
-        <a className="underline" href={'/api/docs'}>api docs</a> -
-        <a className="underline" href={'/tos'}>tos</a> -
-        <a className="underline" href={'https://status.modchecker.com'} target="_blank">status</a></p>
+        &nbsp;<span>not affiliated with twitch interactive</span> -
+        &nbsp;<a className="underline" href={'/api/docs'}>api docs</a> -
+        &nbsp;<a className="underline" href={'/tos'}>tos</a> -
+        &nbsp;<a className="underline" href={'https://status.modchecker.com'} target="_blank">status</a></p>
     </footer>
   );
 };
