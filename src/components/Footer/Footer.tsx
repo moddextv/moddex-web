e39c2ded94b97b66a794c17464b7f50ed86c309c
maersux <marcel@gertsch.be>
2024-08-07 14:13:29 +0200

    style: footer width

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,17 +3,19 @@ export const Footer = () => {
 
   return (
     <footer className="bg-primary-800 py-4 text-center">
-      <p>
-        <span>&copy; {year}</span>
-        <span> | </span>
-        <span>not affiliated with twitch interactive</span>
-        <span> | </span>
-        <a className="underline" href={'/tos'}>tos</a>
-        <span> | </span>
-        <a className="underline" href={'/api/docs'}>api docs</a>
-        <span> | </span>
-        <a className="underline" href={'https://status.modchecker.com'} target="_blank">status</a>
-      </p>
+      <div className="max-w-[1024px] mx-auto px-6">
+        <p>
+          <span>&copy; {year} modchecker</span>
+          <span> | </span>
+          <span>not affiliated with twitch interactive</span>
+          <span> | </span>
+          <a className="underline" href={'/tos'}>tos</a>
+          <span> | </span>
+          <a className="underline" href={'/api/docs'}>api docs</a>
+          <span> | </span>
+          <a className="underline" href={'https://status.modchecker.com'} target="_blank">status</a>
+        </p>
+      </div>
     </footer>
   );
 };
